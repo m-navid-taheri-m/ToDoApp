be1741299622c251b22956efe38e62dbf9d6a121
GooFy <GooFy@DESKTOP-VNCBOFR>
2022-10-28 10:35:54 +0330

    Add project files.

---
+++ CRUD.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{       public class Crud
+    {
+       
+
+        public static void Create(string? myTask, ref List<string?> myList)
+        {
+
+           
+   
+            try
+            {
+                myList.Add(myTask);
+
+                Console.WriteLine("\n");
+                Console.WriteLine("\n");
+                Console.WriteLine($"Task {myTask} has been successfully added  to your list.");
+                Console.WriteLine("\n");
+                Show(myList);
+                Console.WriteLine("\n");
+
+                Console.WriteLine("count :" + myList.Count());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               
+            }
+
+
+        }
+
+
+        public static void Update(ref List<string?> myList)
+        {
+            Console.WriteLine("Enter the index of the task that you want to update : \n");
+            var myListIndex = Convert.ToInt32(Console.ReadLine());
+            
+            var strings = myList.ToArray();
+
+            Console.WriteLine("Enter any text to update the index : ");
+            strings[myListIndex] = Console.ReadLine();
+            myList = strings.ToList();
+
+            Console.WriteLine($"The task on index {myListIndex}  has been updated ");
+
+            Show(myList);
+
+
+        }
+       public static void Delete(ref List<string?> myList)
+        {
+
+            Console.WriteLine("Enter the index of the task that you want to be deleted ?");
+            var myIndex = Convert.ToInt32(Console.ReadLine());
+            myList.RemoveAt(myIndex);
+
+            Console.WriteLine($"The task {myIndex} has been deleted successfully !");
+            Show(myList);
+
+        }
+
+       public static void Show(List<string?> myList)
+       {
+             Console.WriteLine("Your tasks so far : ");
+
+            foreach (var variable in myList) 
+                Console.WriteLine(variable + "[" + myList.IndexOf(variable) + "]");
+
+
+       }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Channels;
+
+namespace ToDoApp;
+
+internal class Program
+{ 
+    
+    private static void Main(string[] args)
+    {
+        
+        var endApp = false;
+
+        var myList = new List<string?>();
+
+        var crud = new Crud();
+
+        Console.WriteLine("\t\t\t------------- Welcome to the ToDoApp! -------------\t\t\t");
+        while (!endApp)
+        {
+            
+            Console.WriteLine("Choose your options : ");
+            Console.WriteLine("\tc -Create a task");
+            Console.WriteLine("\td -Delete a task");
+            Console.WriteLine("\tu -Update a task");
+            Console.WriteLine("\ts -Show your list");
+            var option = Console.ReadLine() ?? throw new InvalidOperationException();
+
+
+            switch (option)
+            {
+
+                case "s":
+                    Crud.Show(myList);
+                    break;
+                case "c":
+
+                    Console.WriteLine("Enter any text to be added as your task :");
+                            Crud.Create(Console.ReadLine()!, ref myList);                
+                    break;
+                case "d":
+                    Crud.Delete(ref myList);
+                    break;
+                case "u":
+
+                    
+                    Crud.Update(ref myList);
+                    break;
+              
+              
+            }
+        }
+
+
+    }
+}
